4ff85de7236b2731c213b3bd082617cc69dc1a10
Shahzaib Khan <shahzaib.khan0217@gmail.com>
2022-10-08 15:54:35 +0500

    Update app.js

--- Server Side Programming/Nodejs/app.js
+++ Server Side Programming/Nodejs/app.js
@@ -1,5 +1,27 @@
 const http = require('http');
-// http protocol is used to handle server request and response
+// http protocol is used to handle client's request and server's response
 
-// create server takes functions as a parameter
-http.createServer().listen(4500);
+/* createServer() takes functions as a parameter,
+I've passed an arrow fun() with 2 parameters req,res
+*/
+/* req: it receives user request from url and 
+access it in Node.js. user request can be a form 
+submission or any search query
+*/
+/* res: is the resonse sent to the client */
+http.createServer((req, res) => {
+    res.write("<h1>Hi this is Shahzaib khan</h1>");
+    res.end(); // to tell Nodejs where to stop
+}).listen(4500);
+
+/* Second Method
+We can define a function() separately and can pass
+ its name as a parameter in createServer()
+ 
+ const datacontrol = (req, res) => {
+    res.write("<h1>Hi this is Shahzaib khan</h1>");
+    res.end(); // to tell Nodejs where to stop
+} 
+ http.createServer(datacontrol).listen(4500);
+ */
+
