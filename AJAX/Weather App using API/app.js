7f293b00b355d7a71e7b0d1a2a03415584730088
Shahzaib Khan <shahzaib.khan0217@gmail.com>
2022-10-18 00:17:17 +0500

    Weather app updated

--- AJAX/Weather App using API/app.js
+++ AJAX/Weather App using API/app.js
@@ -12,27 +12,35 @@ const getWeather = (cityName) => {
     const api_endpoint = "https://api.openweathermap.org/data/2.5/weather?";
     const api_key = "efdc720d1d913e6c8f75c0bf6686e5e6";
     // units=metric will show temp in Celcius
-    const API = api_endpoint + "q=" + cityName + "&appid=" + api_key + "&units=metric";
+    var API = api_endpoint + "q=" + cityName + "&appid=" + api_key + "&units=metric";
 
     //setting up request using AJAX
     let xhr = new XMLHttpRequest();
-
-    //action when data is returned 
-    xhr.addEventListener("load", responseHandler);
-    // fro JSON data
-    xhr.responseType = "json";
     //open connection
     xhr.open("GET", API);
+    // for JSON data
+    xhr.responseType = "json";
+
+    //action when data is returned (ready state is Done(4) or status 200)
+    xhr.onload = function () {
+        if (this.status == 200) {
+            console.log(API);
+            const data = this.response;
+            let inner_html = `
+            <p>City: ${data.name}</p>
+            <p>Temprature: ${data.main.temp}</p>
+            <p>Feels Like: ${data.main.feels_like}</p>
+            <p>Description: ${data.weather[0].description}</p>
+            <p>Min. Temp: ${data.main.temp_min}</p>
+            <p>Max. Temp: ${data.main.temp_max}</p>
+            `;
+            document.getElementById("weatherInfo").innerHTML = inner_html;
+
+        }
+        else {
+            alert("Unable to Load Data");
+        }
+    }
     //send data
     xhr.send();
-}
-
-// This function is called when data is received
-const responseHandler = () => {
-    if (this.status == 200) { //data received successfully
-
-    }
-    else { //error in data receiving
-
-    }
 }
